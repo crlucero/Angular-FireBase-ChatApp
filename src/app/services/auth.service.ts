82aaf603ce5cea5c12ed76c9c4be958dc9c87e0a
Guest User <Guest@epicodus-118.local>
2019-01-31 15:21:18 -0800

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class AuthService {
